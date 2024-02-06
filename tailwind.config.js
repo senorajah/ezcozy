413aaee7548f6657b29663ad61eab6e0f02d736a
Rajah Seno <rajahseno6@gmail.com>
2024-02-07 00:17:19 +0800

    designed login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["./ezcozyPage/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        protest:'Protest Riot',
+        dancing:'Dancing Script'
+      },
+      colors: {
+        navyblue: '#045174',
+        dessrtSun: '#D89C60',
+        burntOrange: '#E87A00',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
 
