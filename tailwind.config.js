61cf1bf83aa8e05fe2496d53428497120baa4b59
Rajah Seno <rajahseno6@gmail.com>
2024-02-07 22:59:28 +0800

    redesigned login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./ezcozyPage/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
-        protest:'Protest Riot',
-        dancing:'Dancing Script'
+      fontFamily: {
+        protest: 'Protest Riot',
+        dancing: 'Dancing Script'
       },
       colors: {
         navyblue: '#045174',
-        dessrtSun: '#D89C60',
+        dessrtsun: '#D89C60',
         burntOrange: '#E87A00',
-      }
+      },
+      backgroundImage: theme => ({
+        'bg1': "url('C:/Users/rajah/Downloads/schoolFiles/ezcozy/pic/bg.jpg')",
+         'card': "url(C:/Users/rajah/Downloads/schoolFiles/ezcozy/pic/cardbg.jpg)",
+      })
     },
   },
-  plugins: [
-
-  ],
+  plugins: [],
 }
-
