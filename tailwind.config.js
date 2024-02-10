e95cacb1c50fd571fd7f243501028fd3c3f2ec20
Rajah Seno <rajahseno6@gmail.com>
2024-02-11 00:57:08 +0800

    created register page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,20 @@ module.exports = {
     extend: {
       fontFamily: {
         protest: 'Protest Riot',
-        dancing: 'Dancing Script'
+        dancing: 'Dancing Script',
+        oswald: 'Oswald'
       },
       colors: {
         navyblue: '#045174',
         dessrtsun: '#D89C60',
         burntOrange: '#E87A00',
+        coral: '#F69770'
       },
       backgroundImage: theme => ({
         'bg1': "url('C:/Users/rajah/Downloads/schoolFiles/ezcozy/pic/bg.jpg')",
          'card': "url(C:/Users/rajah/Downloads/schoolFiles/ezcozy/pic/cardbg.jpg)",
+         'logo1': "url(C:/Users/rajah/Downloads/schoolFiles/ezcozy/pic/logo1.png)",
+         'logo2': "url(C:/Users/rajah/Downloads/schoolFiles/ezcozy/pic/logowhite.png)"
       })
     },
   },
